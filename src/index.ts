7f283ddde4d13032618a47f691b7ce5b37bce1b7
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2020-07-20 09:30:10 +0200

    updated

--- src/index.ts
+++ src/index.ts
@@ -41,13 +41,13 @@ const init = (idNumber: number) => {
    * Get Control number
    * The last digit is a control number
    */
-  controlNumber = +idNumber.toString().split("")[12];
+  controlNumber = +idNumber.toString().split("")?.[12];
 
   // Get odd and even values
   setValidate(idNumberArray);
 };
 
-function setValidate(idNumberArray: string[]) {
+const setValidate = (idNumberArray: string[]) => {
   idNumberArray.forEach((value, index) => {
     // If Odd
     if (index % 2 == 0) {
@@ -66,10 +66,10 @@ function setValidate(idNumberArray: string[]) {
   const compareControl = totalEven + totalOdd;
 
   // Get Calculated Control
-  calculatedControlNumber = 10 - +compareControl.toString().split("")[1];
+  calculatedControlNumber = 10 - +compareControl.toString().split("")?.[1];
 }
 
-function getTotalEvenDigits() {
+const getTotalEvenDigits = () => {
   const totalEvenDigit = (+totalMergedEven * 2).toString().split("");
 
   // Get total Even numbers
