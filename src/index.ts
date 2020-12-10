4e9bc296120505046fa6b1c266afe2c0c8784223
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2020-12-10 20:58:23 +0200

    Code Refactor

--- src/index.ts
+++ src/index.ts
@@ -3,65 +3,55 @@
  *
  * How SA ID Works
  *
- * YYMMDDGSSSCAZ
+ * YYMMDDGSSSCAZ 
  *
- * YYMMDD : Date of birth (DOB)
- * G : Gender. 0-4 Female; 5-9 Male
- * SSS : Sequence No. for DOB/G combination
- * C : Citizenship. 0 SA; 1 Other
- * A : Usually 8, or 9 but can be other values
- * Z : Calculated control (check) digit
+ * YYMMDD : Date of birth (DOB) 
+ * G : Gender. 0-4 Female; 5-9 Male 
+ * SSS : Sequence No. for DOB/G combination 
+ * C : Citizenship. 0 SA; 1 Other 
+ * A : Usually 8, or 9 but can be other values 
+ * Z : Calculated control (check) digit 
  *
  * Ref: https://dirkstrauss.com/south-african-id-number-validation-in-c/
  *
  */
-let totalOdd = 0;
-let totalEven = 0;
-let totalMergedEven = "";
-let controlNumber: number = 0;
-let calculatedControlNumber: number = 0;
 
 /**
  * Validate ID Number
  */
 export const validate = (idNumber: number) => {
-  init(idNumber);
-  return calculatedControlNumber === controlNumber;
+  return calculateControlNumber(idNumber) === controlNumber(idNumber);
 };
 
+
 /**
- * Calculates Odd and Even numbers
- *
- */
-const init = (idNumber: number) => {
-  /**
    * Get Control number
    * The last digit is a control number
    */
-  controlNumber = parseInt(idNumber.toString().split("")?.[12]);
+const controlNumber = (idNumber: number) => {
+  return parseInt(idNumber.toString().split("")?.[12]);
+}
 
-  if (idNumber.toString().length >= 12) {
-    // Reset
-    calculatedControlNumber = 0;
-
-    // Split ID number into an array
-    const idNumberArray = idNumber.toString().split("", 12);
-
-    // Get odd and even values
-    setValidate(idNumberArray);
-  }
-};
-
-const setValidate = (idNumberArray: string[]) => {
+/**
+ * 
+ * @param idNumberArray 
+ * return Compare Control Number
+ */
+const calculateControlNumber = (idNumber: number) => {
   // Reset
-  totalOdd = 0;
-  totalMergedEven = "";
-  totalEven = 0;
+  let totalOdd = 0;
+  let totalMergedEven = "";
+  let totalEven = 0;
 
+  // Split ID Number 
+  const idNumberArray = idNumber.toString().split("", 12);
+
+  // Add Odd and Even number
   idNumberArray.forEach((value, index) => {
     // If Odd
     if (index % 2 == 0) {
       totalOdd += parseInt(value);
+
     } // If even
     else {
       // Concatenate all even values
@@ -69,22 +59,29 @@ const setValidate = (idNumberArray: string[]) => {
     }
   });
 
+
   // Get Event Digit and Split
-  getTotalEvenDigits();
+  totalEven = getTotalEven(totalMergedEven);
 
   // Add all numbers
   const compareControl = totalEven + totalOdd;
 
   // Get Calculated Control
-  calculatedControlNumber =
+  return compareControl % 10 == 0 ? 0 :
     10 - parseInt(compareControl.toString().split("")?.[1]);
-};
 
-const getTotalEvenDigits = () => {
-  const totalEvenDigit = (parseInt(totalMergedEven) * 2).toString().split("");
+};
+/**
+ * Calculates Total Even Digits
+ */
+const getTotalEven = (mergedEven: string) => {
+  const totalEvenDigit = (parseInt(mergedEven) * 2).toString().split("");
+  let even = 0;
 
   // Get total Even numbers
   totalEvenDigit.forEach((value) => {
-    totalEven += parseInt(value);
+    even += parseInt(value);
   });
+
+  return even;
 };
